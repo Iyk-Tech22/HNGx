1f3faf3d84f769073a45e81af66a9eb7e835848a
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2023-09-11 18:26:58 +0100

    Create api route handles

--- stage-two/app/api/persons.py
+++ stage-two/app/api/persons.py
@@ -1,5 +1,22 @@
 from app.api import bp
-from flask import current_app
+from flask import request
 
-@bp.route("/users")
-de
+@bp.route("/<int:id>", methods=["GET"])
+def get_person(id):
+    """ Handle get requests """
+    pass
+
+@bp.route("/<int:id>", methods=["POST"])
+def create_person(id):
+    """ Handle POST requests """
+    pass
+
+@bp.route("/<int:id>", methods=["PUT"])
+def update_person(id):
+    """ Handle PUT requests """
+    pass
+
+@bp.route("/<int:id>", methods=["DELETE"])
+def delete_person(id):
+    """ Handle DELETE requests """
+    pass
